58dee088f55e84fd5b87e1e96e56e8f156438eda
SirTogrul <developmentislifestyle@gmail.com>
2024-04-18 11:25:36 +0400

    fail

---
+++ customcollection/Myllist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customcollection
+{
+    public class Mylist<T>
+    {
+     private int _count;
+        public int  capacity;
+        public List<T> items=new List<T>();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public List<T> FindAll(Predicate<T> predicate)
+        {
+           return items.FindAll(predicate);
+        }
+        public List<T> Find(Predicate<T> predicate,int roomcapacity)
+        {
+            return items.Find(x => x.capacity == roomcapacity);
+        }
+    }
+}
---
+++ customcollection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace customcollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
